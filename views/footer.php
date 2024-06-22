9f1a398dc2a568c08a6e5c172bba5406a3aab614
Pirates-Stuart <165315084+Pirates-Stuart@users.noreply.github.com>
2024-06-22 20:47:15 +0000

    3eme changes

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,35 @@
 <hr>
     <footer>
         <p>&copy; 2024 Mini Site de Vente</p>
+        <div>
+        <ul class="social-link">
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-facebook"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-twitter"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-linkedin"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-instagram"></ion-icon>
+  </a>
+</li>
+
+</ul>
+        </div>
     </footer>
 </body>
 </html>
--- views/services.php
+++ views/services.php
@@ -9,6 +9,36 @@
     <li>Service 3</li>
 </ul>
 
+<div>
+<ul class="social-link">
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-facebook"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-twitter"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-linkedin"></ion-icon>
+  </a>
+</li>
+
+<li class="footer-nav-item">
+  <a href="#" class="footer-nav-link">
+    <ion-icon name="logo-instagram"></ion-icon>
+  </a>
+</li>
+
+</ul>
+</div>
+
 
 
 <?php include 'footer.php'; ?>
