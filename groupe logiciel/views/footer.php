3ea71ad0d2f04058a065c294584c9a78319110a1
Pirates-Stuart <165315084+Pirates-Stuart@users.noreply.github.com>
2024-06-24 18:21:41 +0000

    Update footer.php
    ajout d'icone en bas de la page

--- groupe logiciel/views/footer.php
+++ groupe logiciel/views/footer.php
@@ -1,6 +1,93 @@
 <hr>
-    <footer>
-        <p>&copy; 2024 Mini Site de Vente</p>
+<br>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <!-- Contact information numero github  -->
+            <div class="col-md-4">
+                <h5>Contact Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="https://wa.me/22898576682"><img src="https://img.icons8.com/fluent/48/000000/whatsapp.png" class="icon" alt="WhatsApp"> 228 98 57 66 82</a></li>
+                    <li><a href="mailto:contact@esig.tg"><img src="https://img.icons8.com/fluent/48/000000/email.png" class="icon" alt="start"> esgis</a></li>
+                    <li><a href="https://github.com/hombabe/groupe-logiciel"><img src="https://img.icons8.com/fluent/48/000000/github.png" class="icon" alt="GitHub"> github.com/hombabe/groupe-logiciel</a></li>
+                </ul>
+            </div>
+            <!-- site icon -->
+            <div class="col-md-4 text-center">
+                <img  src="public/images/irt-shop-high-resolution-logo.svg" alt="Site Icon" style="width: 100px;border-radius:12px;">
+                <p>IRT shop</p>
+            </div>
+
+            <!-- les images des article  -->
+            <div class="col-md-4">
+                <h5>Featured Articles</h5>
+                <div class="row">
+                    <div class="col-6 col-md-3">
+                        <img src="public/images/montre2.png" alt="Article 1" class="article img-fluid mb-2">
+                    </div>
+                    <div class="col-6 col-md-3">
+                        <img src="public/images/soap.png" alt="Article 2" class="article img-fluid mb-2">
+                    </div>
+                    <div class="col-6 col-md-3">
+                        <img src="public/images/shoe.png" alt="Article 3" class="article img-fluid mb-2">
+                    </div>
+                    <div class="col-6 col-md-3">
+                        <img src="public/images/haut.png" alt="Article 4" class="article img-fluid mb-2">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col text-center">
+                <p> Projet logiciel IRT_ shop </p>
+            </div>
+        </div>
+    </div>
+
+
     </footer>
+   
+<div class="liens" >
+<div class="wrapper">
+    <div class="icon github">
+        <a href="https://github.com/Pirates-Stuart" target="_blank">
+            <div class="tooltip">
+                Piratestuart
+            </div>
+            <span><i class="fab fa-github"></i></span>
+        </a>
+    </div>
+    <div class="icon github">
+        <a href="https://github.com/hombabe" target="_blank">
+            <div class="tooltip">
+                Vivi
+            </div>
+            <span><i class="fab fa-github"></i></span>
+        </a>
+    </div>
+    <div class="icon github">
+        <a href="https://github.com/hombabe" target="_blank">
+            <div class="tooltip">
+                GOMEDark
+            </div>
+            <span><i class="fab fa-github"></i></span>
+        </a>
+    </div>
+    <div class="icon github">
+        <a href="https://github.com/hombabe" target="_blank">
+            <div class="tooltip">
+                Victor
+            </div>
+            <span><i class="fab fa-github"></i></span>
+        </a>
+    </div>
+</div>
+
+</div>
+   
+
+    
 </body>
 </html>
+
