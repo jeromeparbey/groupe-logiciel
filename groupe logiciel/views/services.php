9fc3289994b4fca0291032c1591647c1c902918a
Pirates-Stuart <165315084+Pirates-Stuart@users.noreply.github.com>
2024-06-24 18:26:39 +0000

    Create services.php
    ajout de la page service

---
+++ groupe logiciel/views/services.php
@@ -0,0 +1,67 @@
+<div class="container my-5">
+    <?php include 'header.php'; ?>
+    <h1 class="text-center mb-4">Nos Services</h1>
+    <div class="row">
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 text-center">
+                <img src="public/images/livraison.jpg" class="card-img-top" alt="Service 1">
+                <div class="card-body">
+                    <h5 class="card-title">Livraison</h5>
+                    <p class="card-text">Livraison de vos article a domicile</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 text-center">
+                <img src="public/images/web.png" class="card-img-top" alt="Service 2">
+                <div class="card-body">
+                    <h5 class="card-title">Web desing</h5>
+                    <p class="card-text">Augmenter votre visibiliter</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 text-center">
+                <img src="public/images/projet.jpg" class="card-img-top" alt="Service 3">
+                <div class="card-body">
+                    <h5 class="card-title">business</h5>
+                    <p class="card-text">Travailler avec nous</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center mt-5">
+        <h2>Suivez-nous</h2>
+        <ul class="list-inline">
+            <li class="list-inline-item">
+                <a href="#" class="text-dark">
+                    <i class="fab fa-facebook fa-2x"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="text-dark">
+                    <i class="fab fa-twitter fa-2x"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="text-dark">
+                    <i class="fab fa-linkedin fa-2x"></i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="#" class="text-dark">
+                    <i class="fab fa-instagram fa-2x"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <?php include 'footer.php'; ?>
+</div>
+
+<!-- Scripts Bootstrap et Font Awesome -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
